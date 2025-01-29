0daeb25cbb03277639dbc309383b33762f53823c
JoanMarin <joanmarinbustamante@gmail.com>
2025-01-29 16:05:58 +0000

    [FIX] def unlink() in partner_multi_email module

--- partner_multi_email/models/res_partner_email.py
+++ partner_multi_email/models/res_partner_email.py
@@ -32,3 +32,11 @@ class ResPartnerEmail(models.Model):
             state = self.search([], limit=limit)
 
         return state.name_get()
+
+    @api.multi
+    def unlink(self):
+        for record in self:
+            if record.is_main:
+                record.partner_id.email = False
+
+        return super(ResPartnerEmail, self).unlink()
