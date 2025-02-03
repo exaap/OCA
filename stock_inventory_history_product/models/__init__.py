70f64fc0878efd1384bcaf5ae0ed49318e411f83
JoanMarin <joanmarinbustamante@gmail.com>
2025-02-03 22:44:39 +0000

    [ADD] stock_inventory_history_product module

---
+++ stock_inventory_history_product/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2024 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0).
+
+from . import product_product
+from . import product_template
+from . import stock_inventoy_line
+from . import stock_inventoy
---
+++ stock_inventory_history_product/models/stock_inventoy.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0).
+
+from odoo import models, api
+
+
+class StockInventory(models.Model):
+    _inherit = "stock.inventory"
+
+    @api.multi
+    def post_inventory(self):
+        res = super(StockInventory, self).post_inventory()
+
+        for move_id in self.move_ids:
+            move_id.product_id.last_inventory_date = move_id.date
+
+        return res
