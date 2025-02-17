297db78268aad928c01724ea7edad0ba0d912165
JoanMarin <joanmarinbustamante@gmail.com>
2025-02-17 23:13:52 +0000

    [ADD] base_location_district module

---
+++ base_location_district/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2024 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0).
+
+{
+    "name": "Base Location Districts",
+    "version": "12.0.1.0.0",
+    "category": "Partner Management",
+    "author": "EXA Auto Parts Github@exaap, "
+    "Joan Marín Github@JoanMarin, "
+    "Odoo Community Association (OCA)",
+    "maintainers": ["joanmarin"],
+    "website": "https://github.com/OCA/partner-contact",
+    "depends": [
+        "base_location",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_city_zip_district_views.xml",
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
---
+++ base_location_district/models/res_partner.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0).
+
+from odoo import models, fields, api
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    district_id = fields.Many2one(
+        comodel_name="res.city.zip.district", string="District"
+    )
+
+    @api.onchange("district_id")
+    def onchange_district(self):
+        if self.district_id:
+            self.zip_id = self.district_id.zip_id
