1aed7713009b779a38357cc7882cade677c6da78
JoanMarin <joanmarinbustamante@gmail.com>
2025-08-21 03:28:54 +0000

    [ADD] _onchange_product_id def

--- account_product_set/models/product_set_sale.py
+++ account_product_set/models/product_set_sale.py
@@ -1,7 +1,7 @@
 # Copyright 2024 Joan Marín <Github@JoanMarin>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0).
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ProductSetSale(models.Model):
@@ -13,9 +13,15 @@ class ProductSetSale(models.Model):
         comodel_name="product.product",
         string="Kit",
         domain=[("set_line_ids", "!=", False)],
+        required=True,
     )
-    product_qty = fields.Float("Quantity")
+    product_qty = fields.Float(string="Quantity", default=0)
     price_unit = fields.Float(string="Price Unit")
+
+    @api.onchange("product_id")
+    def _onchange_product_id(self):
+        for rec in self:
+            rec.price_unit = rec.product_id.lst_price
 
     def name_get(self):
         res = []
