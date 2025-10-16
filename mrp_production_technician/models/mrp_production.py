8af1d1c637742dc764f20decf5a77e479fa92303
JoanMarin <joanmarinbustamante@gmail.com>
2025-10-16 15:53:44 +0000

    [FIX] mrp_production_technician module

--- mrp_production_technician/models/mrp_production.py
+++ mrp_production_technician/models/mrp_production.py
@@ -8,3 +8,6 @@ class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
     user_technician_id = fields.Many2one(comodel_name="res.users", string="Technician")
+    user_machining_technician_id = fields.Many2one(
+        comodel_name="res.users", string="Machining Technician"
+    )
