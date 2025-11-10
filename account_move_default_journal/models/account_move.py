7a2b4bfd39e7eff6311ae60607795883fbd626f1
JoanMarin <joanmarinbustamante@gmail.com>
2025-11-10 09:23:42 +0000

    [ADD] account_move_default_journal module

---
+++ account_move_default_journal/models/account_move.py
@@ -0,0 +1,35 @@
+# Copyright 2021-2023 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    @api.model
+    def _search_default_journal(self):
+        company_id = self._context.get("default_company_id", self.env.company.id)
+        company = self.env["res.company"].browse(company_id)
+        journal_types = self._get_valid_journal_types()
+
+        # Use the appropriate config parameter for the journal type
+        journal = None
+
+        if "sale" in journal_types:
+            journal = company.default_sale_journal_id
+        elif "purchase" in journal_types:
+            journal = company.default_purchase_journal_id
+        elif "general" in journal_types:
+            journal = company.default_general_journal_id
+
+        # Use the found journal, otherwise default back to normal behavior
+        default_currency_id = self._context.get("default_currency_id")
+
+        if (
+            not default_currency_id
+            or not journal
+            or journal.currency_id.id == default_currency_id.id
+        ):
+            return journal
+
+        return super()._search_default_journal()
