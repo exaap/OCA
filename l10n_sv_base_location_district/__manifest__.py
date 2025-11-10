24901654527224cd762a98e82d279733f567433d
JoanMarin <joanmarinbustamante@gmail.com>
2025-11-10 09:30:39 +0000

    [ADD] l10n_sv_base_location_district module

---
+++ l10n_sv_base_location_district/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2024 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0).
+
+{
+    "name": "El Salvador Data Districts",
+    "version": "16.0.1.0.0",
+    "category": "Localization",
+    "author": "EXA Auto Parts Github@exaap, "
+    "Joan Marín Github@JoanMarin, "
+    "Odoo Community Association (OCA)",
+    "maintainers": ["joanmarin"],
+    "website": "https://github.com/OCA/l10n-el-salvador",
+    "depends": [
+        "base_location_district",
+        "l10n_sv_base_location",
+    ],
+    "data": [
+        "data/res.city.zip.district.csv",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
